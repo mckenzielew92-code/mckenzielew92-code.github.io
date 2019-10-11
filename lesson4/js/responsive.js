36867830aa8491077d5b19da17aac7d2d39f9c01
mckenzielew92-code <mckenzielew92@byui.edu>
2019-10-10 21:05:43 -0600

    update

--- lesson4/js/responsive.js
+++ lesson4/js/responsive.js
@@ -1,4 +1,21 @@
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0]
-    .classList.toggle("responsive");
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+
+var today = new Date()
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var dow = weekday[today.getDay()];
+var dd =  today.getDate()
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+
+today = dow + ", " + mm + " " + dd + ", " + yyyy;
+document.getElementById("todays-date").innerHTML = today;
