f36583e4c20e16f349d1904fe416b4a6f7d727a0
mckenzielew92-code <mckenzielew92@byui.edu>
2019-10-24 13:41:20 -0600

    update

---
+++ lesson6/js/todaysdate.js
@@ -0,0 +1,30 @@
+var today = new Date()
+var weekday = new Array(7);
+weekday [0] = "Sunday";
+weekday [1] = "Monday";
+weekday [2] = "Tuesday";
+weekday [3] = "Wednesday";
+weekday [4] = "Thursday";
+weekday [5] = "Friday";
+weekday [6] = "Saturday";
+var month = new Array(12);
+month [0] = "Jan";
+month [1] = "Feb";
+month [2] = "Mar";
+month [3] = "Apr";
+month [4] = "May";
+month [5] = "Jun";
+month [6] = "Jul";
+month [7] = "Aug";
+month [8] = "Sep";
+month [9] = "Oct";
+month [10] = "Nov";
+month [11] = "Dec";
+var dow = weekday[today.getDay()];
+var mm = month[today.getMonth()];
+var dd = today.getDate();
+var yyyy = today.getFullYear();
+
+today = " " + dow + ", " + dd + " " + mm + " " + yyyy;
+document.getElementById("todaysdate").innerHTML = today;
+
