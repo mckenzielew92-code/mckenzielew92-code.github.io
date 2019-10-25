8c02fcdc5427e3c91a84a3f4b579ac16a97c6956
mckenzielew92-code <mckenzielew92@byui.edu>
2019-10-25 15:13:27 -0600

    update java

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,17 @@
-var temp = parseFloat(document.getElementsByClassName('high').innherhtml);
-var speed = parseFloat(document.getElementsByClassName('speed').innerhtml);
+
+function newChill(){
+var temp = parseFloat(document.getElementById('high').innerHTML);
+var speed = parseFloat(document.getElementById('speed').innerHTML);
 var chill = windChill (temp, speed);
 //var display = result;
-document.getElementsByClassName('windchill').innerHTML = chill;
 
+document.getElementById('windchill').innerHTML = chill;
+}
 
 
 
     function windChill(temp, speed){
-        var f = 'NaN';
+        var f = 'mckenzie';
         if (temp <= 50 && speed > 3) {  
             f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
         }
