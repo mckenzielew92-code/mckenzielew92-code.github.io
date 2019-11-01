4e1e94b95af190e22ec0c64e2d96495843396102
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-01 06:16:15 -0600

    update

---
+++ lesson7/banner.js
@@ -0,0 +1,7 @@
+function pancakeDay(){
+    var wd = new Date().getDay();
+    
+    if (wd == 5) {
+        document.getElementById("pancakes").innerHTML = "Come join us for pancakes in the park!  Preston Downtown Park, 9 A.M. EVERY Friday!"
+    }
+    }
---
+++ lesson7/webfontload.js
@@ -0,0 +1,7 @@
+WebFont.load({
+    google: {
+        families: [
+            "Noto Sans", "Playfair Display"
+        ]
+    }
+});
