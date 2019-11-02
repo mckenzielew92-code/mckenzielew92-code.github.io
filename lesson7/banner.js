689ade9828548fc8c12342295a104c6beb030f23
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-02 10:05:49 -0600

    update js

--- lesson7/banner.js
+++ lesson7/banner.js
@@ -1,8 +1,8 @@
 function pancakeDay(){
     var wd = new Date().getDay();
     
-    if (wd == 5) {
-        document.getElementById("pancakes").innerHTML = "Come join us for pancakes in the park!  Preston Downtown Park, 9 A.M. EVERY Friday!";
+    if (wd == 6) {
+        document.getElementById("pancakes").innerHTML = "Come join us for pancakes in the park!  Preston Downtown Park, 9 A.M. EVERY Saturday!";
         document.getElementById('pancakes').style.display="block";
     }
     }
