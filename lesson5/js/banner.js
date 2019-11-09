94ea9a929937ec29ea86493c7aa4f733135b8c83
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-08 22:12:22 -0700

    updates

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,7 +1,9 @@
 function pancakeDay(){
-var wd = new Date().getDay();
-
-if (wd == 5) {
-    document.getElementById("pancakes").innerHTML = "Come join us for pancakes in the park!  Preston Downtown Park, 9 A.M. EVERY Friday!"
-}
-}
+    var wd = new Date().getDay();
+    
+    if (wd == 5) {
+        document.getElementById("pancakes").innerHTML = "Come join us for pancakes in the park!  Preston Downtown Park, 9 A.M. EVERY Saturday!";
+        document.getElementById('pancakes').style.display="block";
+    }
+    }
+    pancakeDay();
