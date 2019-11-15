ac68ea9160e73e10c53eca83796627cebd1358d2
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-14 21:17:58 -0700

    update

--- lesson9/test.js
+++ lesson9/test.js
@@ -1,45 +1,36 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-
-  .then(function (response) {
+`
+`.then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+   // console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const prophets = jsonObject["prophets"];
 
-
-    for (let i = 0; i < prophets.length; i++ ) {
+    for (let i = 0; i < prophets.length; i++) {
 
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
         let birthdate = document.createElement("p");
         let birthplace = document.createElement("p");
         let image = document.createElement("img");
-
-      
-
+        
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        birthdate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        birthplace.textContent = 'Birth Place: ' + prophets[i].birthplace;
+        birthdate.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+        birthplace.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
 
-        let alt = prophets[i].name + " " + prophets[i].lastname + "-" + i;
-        image.setAttribute('src', prophets[i].imageurl);
-        image.setAttribute("alt",alt);
-      
-        card.appendChild(image);
         card.appendChild(h2);
         card.appendChild(birthdate);
         card.appendChild(birthplace);
+        card.appendChild(image);
+        
+        image.setAttribute('src', prophets[i].imageurl);
+        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order); 
 
+        document.querySelector('div.card').appendChild(card);
 
-      
-        document.querySelector('div.cards').appendChild(card);
-    }
-
-  });
-
-
-  
+  }
+});
