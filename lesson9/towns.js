bec3e07f794a5a9e41b0972f081286cd8f5dd54f
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-15 20:00:00 -0700

    update

---
+++ lesson9/towns.js
@@ -0,0 +1,39 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+
+    .then(function (response){
+        return response.json();
+    })
+const towns = jsonObject['towns'];
+
+for (let i = 0; i < towns.length; i++ ) {
+
+    let card = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let motto = document.createElement("p");
+    let year = document.createElement("p");
+    let pop = document.createElement("p");
+    let rain = document.createElement("p");
+    let image = document.createElement("img");
+
+  
+    h2.textContent = towns[i];
+    motto.textContent = towns[i].motto;
+    year.textContent = 'Year Founded: ' + towns[i].yearFounded;
+    pop.textContent = 'Population:' + towns[i].currentPopulation;
+    rain.textContent = 'Annual Rainfall' + towns[i].averageRainfall;
+    
+
+    image.setAttribute('src', towns[i].photo);
+    image.setAttribute('alt', towns[i].name);
+  
+    card.appendChild(h2);
+    card.appendChild(moto);
+    card.appendChild(year);
+    card.appendChild(pop);
+    card.appendChild(rain);
+  
+    document.querySelector('div.towns').appendChild(card);
+}
+});
