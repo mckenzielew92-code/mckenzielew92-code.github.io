8e995e6920909d57429dfbd8ad30f398e728f113
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-22 19:09:20 -0700

    update

--- lesson10/weatherapi10.js
+++ lesson10/weatherapi10.js
@@ -3,7 +3,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
 
     document.getElementById('weather').textContent = jsObject.id.main;
     document.getElementById('high').textContent = jsObject.main.temp;
@@ -12,7 +12,33 @@ fetch(apiURL)
 
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementsByClassName('').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
+    document.getElementById('weather').textContent = desc;
+
   });
+
+  const jsonURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=bd99fbf52e995e4af1074e56feb731ab";
+
+  fetch(jsonURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+    //console.log(jsObject);
+    for (let i = 0; i < jsObject.list.length; i++)
+      if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+
+        document.getElementById('day1').textContent = jsObect.list[i].main.temp.toFixed(0);
+        document.getElementById('day2').textContent = jsObect.list[i].main.temp.toFixed(0);
+        document.getElementById('day3').textContent = jsObect.list[i].main.temp.toFixed(0);
+        document.getElementById('day4').textContent = jsObect.list[i].main.temp.toFixed(0);
+        document.getElementById('day5').textContent = jsObect.list[i].main.temp.toFixed(0);
+
+
+
+
+     }
+    
+      const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+      document.getElementsByClassName('sun-icon').textContent = imagesrc;  // informational specification only
+      document.getElementsByClassName('sun-icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+      document.getElementsByClassName('sun-icon').setAttribute('alt', desc);
+    
+    });
