200fad1bffe3804ddd1d136b36bcca84f991c004
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-23 19:45:48 -0700

    update

--- lesson10/weatherapi10.js
+++ lesson10/weatherapi10.js
@@ -11,38 +11,36 @@ fetch(apiURL)
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('speed').textContent = jsObject.wind.speed;
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+    const desc = jsObject.weather[0].description; // note how we reference the weather array
     document.getElementById('weather').textContent = desc;
 
   });
 
-  const jsonURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=bd99fbf52e995e4af1074e56feb731ab";
+const jsonURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=bd99fbf52e995e4af1074e56feb731ab";
 
-  fetch(jsonURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
+fetch(jsonURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
     console.log(jsObject);
 
+    
+    let count = 0;
     for (let i = 0; i < jsObject.list.length; i++) {
       if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+        count++;
+    /*Temp*/
+        var temper = jsObject.list[i].main.temp.toFixed(0);
 
-        document.getElementById('day1').textContent = (jsObect.list[i].dt_txt);
-        document.getElementById('day2').textContent = (jsObect.list[i].dt_txt);
-        document.getElementById('day3').textContent = (jsObect.list[i].dt_txt);
-        document.getElementById('day4').textContent = (jsObect.list[i].dt_txt);
-        document.getElementById('day5').textContent = (jsObect.list[i].dt_txt);
+        let myhigh = 'high' + count;
+        document.getElementById(myhigh).textContent = temper;
 
+        /*Day of Week*/
 
-        var high1 = jsObect.list[i].main.temp
+        let myday = 'day' + count;
+        document.getElementById(myday).textContent = (jsObject.list[i].dt_txt);
         
-        document.getElementById('high1').textContent = high1.toFixed(0);
-        document.getElementById('high2').textContent = jsObect.list[i].main.temp.toFixed(0);
-        document.getElementById('high3').textContent = jsObect.list[i].main.temp.toFixed(0);
-        document.getElementById('high4').textContent = jsObect.list[i].main.temp.toFixed(0);
-        document.getElementById('high5').textContent = jsObect.list[i].main.temp.toFixed(0);
-        
-        document.getElementById()
-     }
+
+      }
     }
-    });
+  });
