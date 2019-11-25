8217b24680ab965543a02d0ccd1a20c4f4b43313
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-25 06:34:22 -0700

    upddate

---
+++ lesson11/js/windchill.js
@@ -0,0 +1,25 @@
+
+function newChill(){
+    var temp = parseFloat(document.getElementById('high').innerHTML);
+    var speed = parseFloat(document.getElementById('speed').innerHTML);
+    var chill = windChill (temp, speed);
+    //var display = result;
+    
+    document.getElementById('windchill').innerHTML = chill;
+    }
+    
+    
+    
+        function windChill(temp, speed){
+            var f = 0;
+            if (temp <= 50 && speed > 3) {  
+                f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
+                f = Math.round(f);
+            }
+            else{
+                f = temp;
+                f = Math.round(f);
+            }
+            return f;
+        }
+    
