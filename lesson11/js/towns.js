0b84cde8743ed7e5d9b8e4235aeb3f7498fa0be7
mckenzielew92-code <mckenzielew92@byui.edu>
2019-11-29 07:22:53 -0700

    update

--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -11,17 +11,20 @@ const towns = jsonObject["towns"];
 
 for (let i = 0; i < towns.length; i++ ) {
 
-    let card = document.createElement("div");
+    if (towns[i].name == "Fish Haven" ||towns[i].name == "Soda Springs" || towns[i].name== "Preston"){
+
+
+    let card = document.createElement("article");
+    let fillCard = document.createElement("section");
     let h2 = document.createElement("h2");
-    let motto = document.createElement("h3");
+    let motto = document.createElement("h4");
     let year = document.createElement("p");
     let pop = document.createElement("p");
     let rain = document.createElement("p");
     let image = document.createElement("img");
 
-    if (towns[i].name == "Fish Haven" ||towns[i].name == "Soda Springs" || towns[i].name== "Preston")
 
-   { h2.textContent = towns[i].name;
+    h2.textContent = towns[i].name;
     motto.textContent = towns[i].motto;
     year.textContent = 'Year Founded: ' + towns[i].yearFounded;
     pop.textContent = 'Population: ' + towns[i].currentPopulation;
@@ -30,12 +33,15 @@ for (let i = 0; i < towns.length; i++ ) {
     image.setAttribute('src', towns[i].photo);
     image.setAttribute('alt', towns[i].name);
     
-    card.appendChild(h2);
-    card.appendChild(motto);
-    card.appendChild(year);
-    card.appendChild(pop);
-    card.appendChild(rain);
+    
+    fillCard.appendChild(h2);
+    fillCard.appendChild(motto);
+    fillCard.appendChild(year);
+    fillCard.appendChild(pop);
+    fillCard.appendChild(rain);
+    card.appendChild(fillCard);
     card.appendChild(image);
+    card.classList.add("town-borders", "town-content");
   
     document.querySelector('div.towns').appendChild(card);
    }
@@ -44,3 +50,4 @@ for (let i = 0; i < towns.length; i++ ) {
     
 }
 });
+
