74c9825540ca3506fb729f261aa7efb56c7f95d0
mckenzielew92-code <mckenzielew92@byui.edu>
2019-12-10 21:25:47 -0700

    update

--- term-project/script/script.js
+++ term-project/script/script.js
@@ -2,3 +2,7 @@
 let d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
 
+document.querySelector('meta[name=description]').setAttribute('content', document.querySelector('meta[name=description]').getAttribute('content') + ' weatherapi.js');
+
+//
+
