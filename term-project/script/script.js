9e875f63a5b06cb2ed476cd834080dcff810e7f7
mckenzielew92-code <mckenzielew92@byui.edu>
2019-12-12 15:16:39 -0700

    update

--- term-project/script/script.js
+++ term-project/script/script.js
@@ -2,7 +2,7 @@
  let d = new Date();
  document.getElementById("year").innerHTML = d.getFullYear();
 
- document.querySelector('meta[name=description]').setAttribute('content', document.querySelector('meta[name=description]').getAttribute('content') + ' weatherapi.js');
+ document.querySelector('meta[name=description]').setAttribute('content', document.querySelector('meta[name=description]').getAttribute('content') + ' script.js');
 
  //Responsive Menu
  function toggleMenu() {
